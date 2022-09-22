1ee37fd91babd0a03f809b76568312e4cdd2463d
amenasyed <80727252+amenasyed@users.noreply.github.com>
2022-09-22 14:11:08 -0400

    first commit

---
+++ build/precache-manifest.ef4b9edd751ac969971a5a851ea0acca.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "f42802d8a871e4b71e08",
+    "url": "/portfolio/static/css/main.d0402bfb.chunk.css"
+  },
+  {
+    "revision": "f42802d8a871e4b71e08",
+    "url": "/portfolio/static/js/main.f42802d8.chunk.js"
+  },
+  {
+    "revision": "b462f22b128ccd7b415b",
+    "url": "/portfolio/static/js/1.b462f22b.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "c2fd6c9bcb4c7deaf798f1c64d0fb442",
+    "url": "/portfolio/static/media/amena-resume-2022.c2fd6c9b.pdf"
+  },
+  {
+    "revision": "d5b12d5f5aadf6ae52eed876cfeb36de",
+    "url": "/portfolio/static/media/deciSense.d5b12d5f.png"
+  },
+  {
+    "revision": "5c43b348509f7f63ab0a4d33a92e0c60",
+    "url": "/portfolio/static/media/moodFM.5c43b348.png"
+  },
+  {
+    "revision": "61588d2561500f20c9c0862637a732f2",
+    "url": "/portfolio/static/media/me.61588d25.png"
+  },
+  {
+    "revision": "712b980a491217c50a7033fd43c19b70",
+    "url": "/portfolio/index.html"
+  }
+];
