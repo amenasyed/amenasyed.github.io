df2e2e4cf7396a54a7dab09a3906a1c3e736ab8b
amenasyed <80727252+amenasyed@users.noreply.github.com>
2022-09-22 14:29:27 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/amenasyed.github.io/precache-manifest.bea45db3e0394e8704b70de13473c5dc.js"
+  "/precache-manifest.f1073744736156067895e02fb75c4ac1.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/amenasyed.github.io/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
